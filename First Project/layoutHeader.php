a04798a1ded683e5bbefd118d8b36ebd1da6e32c
tojas123 <hodositamas00@gmail.com>
2026-09-05 11:15:41 +0200

    i did some work on nba statistics site in first project, added css, a font and worked on the header

--- First Project/layoutHeader.php
+++ First Project/layoutHeader.php
@@ -1,8 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>NBA tracker</title>
 </head>
+
 <body>
+    <header>
+        <h1>NBA tracker</h1>
+        <h4>Track live games, look at player stats by names, etc</h4>
+    </header>
